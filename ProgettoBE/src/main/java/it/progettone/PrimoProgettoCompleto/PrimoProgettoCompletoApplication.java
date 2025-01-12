c95c2facd6d0d796b07061a8e5644c894fa6cd79
Riccardo Santilli <riccardosantilli@live.it>
2025-01-12 11:33:11 +0100

    aaaa

--- ProgettoBE/src/main/java/it/progettone/PrimoProgettoCompleto/PrimoProgettoCompletoApplication.java
+++ ProgettoBE/src/main/java/it/progettone/PrimoProgettoCompleto/PrimoProgettoCompletoApplication.java
@@ -2,12 +2,13 @@ package it.progettone.PrimoProgettoCompleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"it.progettone.PrimoProgettoCompleto.core", "it.progettone.PrimoProgettoCompleto.web"})
 public class PrimoProgettoCompletoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimoProgettoCompletoApplication.class, args);
 	}
-
 }
