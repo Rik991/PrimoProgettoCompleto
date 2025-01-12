ac330cb6bfa6e9e65f2d4619cb567673137f15cf
Riccardo Santilli <riccardosantilli@live.it>
2025-01-12 10:33:21 +0100

    Initial commit

---
+++ ProgettoBE/src/main/java/it/progettone/PrimoProgettoCompleto/PrimoProgettoCompletoApplication.java
@@ -0,0 +1,13 @@
+package it.progettone.PrimoProgettoCompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoProgettoCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoProgettoCompletoApplication.class, args);
+	}
+
+}
---
+++ ProgettoBE/src/test/java/it/progettone/PrimoProgettoCompleto/PrimoProgettoCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.progettone.PrimoProgettoCompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoProgettoCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
